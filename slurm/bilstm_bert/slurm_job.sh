4d1af7bfee2bf12918914965d7f104aa51c79150
Benjamin <benjamin.tan.chin.han@gmail.com>
2023-11-11 17:11:06 +0800

    updated shell scripts to follow new flags

--- slurm/bilstm_bert/slurm_job.sh
+++ slurm/bilstm_bert/slurm_job.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#SBATCH --time=180
+#SBATCH --time=4320
 #SBATCH --job-name=g14_bilstm
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=e0543676@comp.nus.edu.sg
 #SBATCH --gpus=v100:1
-#SBATCH --partition=medium
+#SBATCH --partition=long
 #SBATCH -o cs4248_g14.out
 
 chmod +x bilstm.sh
