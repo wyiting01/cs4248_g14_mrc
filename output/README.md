a8f30144fcfe6df68416897f9510a259b24a0e03
Samantha <samanthalowww@gmail.com>
2023-11-15 16:35:42 +0800

    update repo structure

---
+++ output/README.md
@@ -0,0 +1,1 @@
+This model contains all the output json files that contains predictions for dev-v1.1.json and will be used for evaluation.
--- slurm/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-python xlnet.py \
-    --test\
-	--data_path "../data/curated/test_data"\
-	--model_path "./xlnet.pt"\
-    --output_path "./pred.json"
