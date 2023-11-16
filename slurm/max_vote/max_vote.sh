9bd1ee4ab8ea58b1a4b295d75ff5d1dc1bd8cb84
Benjamin <benjamin.tan.chin.han@gmail.com>
2023-11-16 15:57:17 +0800

    removed slurms

--- slurm/max_vote/max_vote.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python ../../src/max_vote.py \
-	--data_path "../../data/curated/test_data" \
-	--bert_path "../../src/bert/model" \
-	--xlnet_model "../../model/xlnet.pt" \
-	--bilstm_model "../../model/bilstm.pt" \
-	--output_path "allMaxVoteAns.json" \
-	--final_output_path "maxVoteAns.json"
